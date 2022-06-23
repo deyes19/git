e62b2d4011c181bcc505f2879b91df253796718c
deyes19 <deyes1219@gmail.com>
2022-06-23 08:43:34 -0500

    añadiendo actualizacion y edicion

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,28 @@ class StudentsController < ApplicationController
             redirect_to new_student_path
         end
     end
+    def edit
+        @student=Student.find(params[:id])
+        
+    end
+    def update
+        @student=Student.find(params[:id])
+        
+        if @student.update(student_params)
+            redirect_to @students
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @student=Student.find(params[:id])
+        @student.destroy
+
+        redirect_to root_path, status: :see_other
+        
+    end
+
     private
     def student_params
         params.require(:student).permit(:name, :lastname, :ndoc)
